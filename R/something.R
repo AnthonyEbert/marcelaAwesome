ff06b6aded9e1f7c7032264a11ce0638496475fe
Anthony <anthonyebert@gmail.com>
2017-11-09 09:02:12 +1000

    Best package ever!!!!!!!!!

---
+++ R/something.R
@@ -0,0 +1,8 @@
+
+#' This function is awesome!
+#' @export
+#' @param x a number
+#' @details This function does awesome things. It's too complex for you to understand.
+marcela_func <- function(x){
+  return(x + 7)
+}
